2117fa18d9b7f993dfcce65345e72ca491881e55
godlikeonline <705285647@qq.com>
2018-12-29 15:44:25 +0800

    阴影设置 命名冲突

--- DropDown/src/DropDown+Appearance.swift
+++ DropDown/src/DropDown+Appearance.swift
@@ -18,10 +18,10 @@ extension DropDown {
 		appearance.selectionBackgroundColor = DPDConstant.UI.SelectionBackgroundColor
 		appearance.separatorColor = DPDConstant.UI.SeparatorColor
 		appearance.cornerRadius = DPDConstant.UI.CornerRadius
-		appearance.shadowColor = DPDConstant.UI.Shadow.Color
-		appearance.shadowOffset = DPDConstant.UI.Shadow.Offset
-		appearance.shadowOpacity = DPDConstant.UI.Shadow.Opacity
-		appearance.shadowRadius = DPDConstant.UI.Shadow.Radius
+		appearance.ddShadowColor = DPDConstant.UI.Shadow.Color
+		appearance.ddShadowOffset = DPDConstant.UI.Shadow.Offset
+		appearance.ddShadowOpacity = DPDConstant.UI.Shadow.Opacity
+		appearance.ddShadowRadius = DPDConstant.UI.Shadow.Radius
 		appearance.animationduration = DPDConstant.Animation.Duration
 		appearance.textColor = DPDConstant.UI.TextColor
         appearance.selectedTextColor = DPDConstant.UI.SelectedTextColor
